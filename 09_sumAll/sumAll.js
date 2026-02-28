c4c4de68fcd6a0f15c2b7d81d72ca13833664541
Mogire <brictine@gmail.com>
2026-02-28 07:37:27 +0300

    completed javascript excercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let initial = 0;
+  let final = 0;
+  let sum = 0;
 
+  if (
+    !Number.isInteger(start) ||
+    !Number.isInteger(end) ||
+    start < 0 ||
+    end < 0
+  ) {
+    return "ERROR";
+  } else {
+    if (start > end) {
+      initial = end;
+      final = start;
+    } else {
+      initial = start;
+      final = end;
+    }
+  }
+
+  for (let i = initial; i <= final; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
